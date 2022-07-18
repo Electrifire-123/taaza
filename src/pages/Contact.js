eb6422399f89009f41fea5ab73007cff27df982e
Electrifire-123 <rahullyadavv77@gmail.com>
2022-07-18 22:03:55 +0530

    updated checkbox

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,158 +1,169 @@
-import React, { useState, useEffect} from 'react'
-import Layout from '../components/Layout'
-import Button from '../components/Button';
+import React, { useState, useEffect } from "react";
+import Layout from "../components/Layout";
+import Button from "../components/Button";
 const Contact = () => {
-    const [user, setUser] = useState({
-        fName: '',
-        lName: '', 
-        mobile: '',
-        email: '',
-        gender: '',
+  const [user, setUser] = useState({
+    fName: "",
+    lName: "",
+    mobile: "",
+    email: "",
+    gender: "",
+  });
+
+  const interests = [
+    {
+      interest: "Science",
+    },
+    {
+      interest: "Sports",
+    },
+    {
+      interest: "Politics",
+    },
+  ];
+  const [checked, setCecked] = useState(new Array(interests.length).fill(false));
+
+  useEffect(() => {
+    const keydownRef = document.addEventListener(
+      "keydown",
+      (e) => e.code === "Enter"
+    );
+
+    return () => {
+      console.log("Component Unmount");
+      document.removeEventListener("keydown", keydownRef);
+    };
+  }, []); // this is component did mount (runs only once)
+
+  // useEffect(() => {
+  //   if(interests.length > 0) console.log('componentDidUpdate');
+  // },[interests]); //
+  // console.log(user)
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+  const submitInt = []
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+    for(let i = 0; i<checked.length; i++){
+      if(checked[i]===true){
+        submitInt.push(interests[i]);
+      }
+    }
+    console.log(submitInt);
+    // logic
+    setUser({
+      fName: "",
+      lName: "",
+      mobile: "",
+      email: "",
+      gender: "",
     });
+  };
 
-    const [interest, setInterest] = useState([])
-
-    useEffect(() => {
-      const keydownRef = document.addEventListener(
-        'keydown',
-        (e) => e.code === 'Enter' 
-      );
-
-      return () => {
-        console.log('Component Unmount');
-        document.removeEventListener('keydown', keydownRef);
-      }
-
-    }, []); // this is component did mount (runs only once)
-
-    // useEffect(() => {
-    //   if(interest.length > 0) console.log('componentDidUpdate');
-    // },[interest]); // 
-    // console.log(user)
-    const handleChange = (e) => {
-        setUser({...user, [e.target.name]: e.target.value});
-       
-      }
-      
-      
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // console.log(user, interest);
-        // logic
-        setUser({
-          fName: '',
-          lName: '', 
-          mobile: '',
-          email: '',
-          gender: '',
-      });
-    }
-
-    const handleInterestChange = (e) => {
-        setInterest([...interest, e.target.value]);
-    }
-    const {fName,lName,email, mobile} =  user;
+  const handleInterestChange = (position) => {
+    const updateChecked = checked.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCecked(updateChecked);
+  };
+  const { fName, lName, email, mobile } = user;
   return (
     <>
-        <Layout>
-            <div className='contact-page'>
-            <form className='input_container' onSubmit={handleSubmit}>
+      <Layout>
+        <div className="contact-page">
+          <form className="input_container" onSubmit={handleSubmit}>
             <section className="input_container">
-                <label>First Name</label>
-                <input
-                  type="text"
-                  value={fName}
-                  onChange={handleChange}
-                  name="fName"
-                  />
-              </section>
-              <section className="input_container">
-                <label>Last Name</label>
-                <input
-                  type="text"
-                  value={lName}
-                  onChange={handleChange}
-                  name="lName"
-                />
-              </section>
-              <section className="input_container">
-                <label>Mobile Number</label>
-                <input
-                  type="number"
-                  value={mobile}
-                  onChange={handleChange}
-                  name="mobile"
-                />
-              </section>
-              <section className="input_container">
-                <label>Email</label>
-                <input
-                  type="email"
-                  value={email}
-                  onChange={handleChange}
-                  name="email"
-                  />
-              </section>
-              <section className="input_container">
-                <label>Gender</label>
-                <input 
-                type="radio" 
-                name="gender" 
+              <label>First Name</label>
+              <input
+                type="text"
+                value={fName}
+                onChange={handleChange}
+                name="fName"
+              />
+            </section>
+            <section className="input_container">
+              <label>Last Name</label>
+              <input
+                type="text"
+                value={lName}
+                onChange={handleChange}
+                name="lName"
+              />
+            </section>
+            <section className="input_container">
+              <label>Mobile Number</label>
+              <input
+                type="number"
+                value={mobile}
+                onChange={handleChange}
+                name="mobile"
+              />
+            </section>
+            <section className="input_container">
+              <label>Email</label>
+              <input
+                type="email"
+                value={email}
+                onChange={handleChange}
+                name="email"
+              />
+            </section>
+            <section className="input_container">
+              <label>Gender</label>
+              <input
+                type="radio"
+                name="gender"
                 value="male"
-                onChange={handleChange}  />
-                Male
-                <input 
-                type="radio" 
-                name="gender" 
+                onChange={handleChange}
+              />
+              Male
+              <input
+                type="radio"
+                name="gender"
                 value="female"
-                onChange={handleChange} />
-                Female
-                <input 
-                type="radio" 
-                name="gender" 
+                onChange={handleChange}
+              />
+              Female
+              <input
+                type="radio"
+                name="gender"
                 value="Other"
-                onChange={handleChange} />
-                Other
-              </section>
-              <section className="input_container">
-                <label>Interest</label>
-                <input 
-                type="checkbox" 
-                name="interest" 
-                value="sports" 
-                id="sports"
-                onChange={handleInterestChange}
-                /> Sports
-                <input 
-                type="checkbox" 
-                name="interest" 
-                value="science" 
-                id="science"
-                onChange={handleInterestChange}
-                />
-                science
-                <input 
-                type="checkbox" 
-                name="interest" 
-                value="politics" 
-                id="politics"
-                onChange={handleInterestChange}
-                />
-                politics
-              </section>
-              <section className="input_container">
-                {/* <button type='submit'>Submit Contact</button> */}
-                <Button buttonText="Submit Contact" type="submit" />
-              </section>
-            </form>
-            </div>
-        </Layout>
-    </>
-  )
-}
+                onChange={handleChange}
+              />
+              Other
+            </section>
+            <h3>Interest</h3>
+            {interests.map(({ interest }, index) => {
+              return (
+                <section key={index} className="input_container">
+                  <input
+                    type="checkbox"
+                    name="interests"
+                    value={`${index}`}
+                    id={`${index}`}
+                    checked={checked[index]}
+                    onChange={() => handleInterestChange(index)}
+                  />
+                  {interest}
+                </section>
+              );
+            })}
 
-export default Contact
+            <section className="input_container">
+              {/* <button type='submit'>Submit Contact</button> */}
+              <Button buttonText="Submit Contact" type="submit" />
+            </section>
+          </form>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default Contact;
 
 // Interview question on removeEventListener()
 
